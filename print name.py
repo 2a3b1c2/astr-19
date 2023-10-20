c18557d7872019fd4f4d7fd9fe38bbf439647f1c
2a3b1c2 <146883291+2a3b1c2@users.noreply.github.com>
2023-10-20 14:32:04 -0700

    Add files via upload

---
+++ print name.py
@@ -0,0 +1,7 @@
+#How to print your name and favorite food
+name="Sharanya Sundaram"
+movie= "Spirited away. It is about a young girl whose parents get turned into pigs, and she has to save them"
+food = "Chips. They are not very healthy, but I like them!"
+print("My name is",name)
+print("My favorite movie is", movie)
+print("My favorite food is", food)
